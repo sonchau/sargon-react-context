c8e1d58a724081595aa9e70cbb90ef8b2312e708
“Son <son.chau@geografia.com.au>
2020-08-08 10:57:09 +1000

    add hover transition

--- src/components/PhotoListItem.js
+++ src/components/PhotoListItem.js
@@ -2,44 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from "styled-components";
 
-const ImageStyled = styled.img`
-    width: 100%;
-    height: auto;
-    padding-bottom: 1rem;
-    grid-row: 1 / 2;
-`;
+
 const ItemStyled = styled.li`
    padding: 1rem;
    background: #f9f7f6;
    border-radius: 1rem;
    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
+   transition: transform .3s;
+   &:hover {
+    transform: translateY(-1rem) scale(1.03); 
+    }
 `;
+const ImageStyled = styled.img`
+    width: 100%;
+    height: auto;
+    padding-bottom: 1rem;
+`;
+
+// const TitleStyled = styled.p`
+//     z-index: 3;
+//     width: 80%;
+//     font-size: 1.6rem;
+//     text-align: center;
+//     padding: 1.25rem;
+//     background-color: #101d2c;
+//     color: #fff;
+//     font-weight: 400;
+//     margin: 0 auto;
+//     transform: translateY(-70%);
+//     grid-area: title;
+ 
+// `;
+
+// const ContentContainerStyled = styled.div`
+// grid-area: content;
+// `;
 
 const ContentStyled = styled.p`
     font-size: 1.5rem;
     font-family: "Josefin Sans", sans-serif;
-    margin: 1rem;
+    margin: 0 1rem 0.5rem;
     color: #c69963;
 `;
-
-const TitleStyled = styled.p`
-    z-index: 3;
-    width: 80%;
-    font-size: 1.6rem;
-    text-align: center;
-    padding: 1.25rem;
-    background-color: #101d2c;
-    color: #fff;
-    font-weight: 400;
-    margin: 0 auto;
-    transform: translateY(-70%);
- 
-`;
-
-// const ContentContainerStyled = styled.div`
-//     transform: translateY(-70%);
-// `;
 const PhotoListItem = ({props}) => {
     //const { link, title, src } = image;
     return  (
@@ -47,10 +51,9 @@ const PhotoListItem = ({props}) => {
         <a target="_blank" rel="noopener noreferrer" href={props.link} title={props.title}>
             <ImageStyled src={props.media.m} alt={props.title}/>
         </a>
-        <TitleStyled>{props.title}</TitleStyled> 
-            <ContentStyled><strong>Author:</strong> {props.author.split('"')[1]}</ContentStyled>
-            <ContentStyled><strong>Tags:</strong> {props.tags}</ContentStyled>
-
+        <ContentStyled><strong>Title:</strong> {props.title}</ContentStyled> 
+        <ContentStyled><strong>Author:</strong> {props.author.split('"')[1]}</ContentStyled>
+        <ContentStyled><strong>Tags:</strong> {props.tags}</ContentStyled>        
     </ItemStyled>
     )
 }
